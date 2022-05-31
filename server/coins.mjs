c1773b1c359f05fe59b7ee72897f66106af08035
Olivier Scherrer <pode.fr@gmail.com>
2022-05-31 16:54:07 +0200

    Add dumb coin server

---
+++ server/coins.mjs
@@ -0,0 +1,60 @@
+const coins = [];
+
+class Coin {
+  #id;
+  #data;
+  #instancesByOwner = [];
+
+  setId(id) {
+    if (this.#id) throw new Error("id is already set and now immutable");
+    this.#id = id;
+  }
+
+  setData(data) {
+    if (this.#data) throw new Error("data is already set and now immutable");
+    this.#data = data;
+  }
+
+  hasOwner(owner) {
+    return this.#instancesByOwner.has(owner);
+  }
+
+  addOwner(owner) {
+    if (this.hasOwner(owner)) {
+      throw new Error("owner already owns this token");
+    }
+
+    this.#instancesByOwner.push(owner);
+  }
+
+  getInstanceByOwner(owner) {
+    const instanceId = this.#instancesByOwner.indexOf(owner);
+
+    if (instanceId > -1) {
+      return instanceId;
+    } else {
+      throw new Error("owner doesn't own this token");
+    }
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get data() {
+    return this.#data;
+  }
+}
+
+export function addCoin(data) {
+  const newCoin = new Coin();
+  const coinId = coins.push(newCoin);
+  newCoin.setId(coinId - 1);
+  newCoin.setData(data);
+
+  return true;
+}
+
+export function getCoinById(id) {
+  return coins[id];
+}
---
+++ server/index.mjs
@@ -0,0 +1,40 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+import { addCoin, getCoinById } from "./coins.mjs";
+
+const schema = buildSchema(`
+    type Query {
+        coin(id: Int): Coin
+    }
+
+    type Coin {
+        id: Int!
+        data: String
+    }
+
+    type Mutation {
+        addCoin(data: String): Boolean
+    }
+`);
+
+const rootValue = {
+  coin: ({ id }) => getCoinById(id),
+  addCoin: ({ data }) => addCoin(data)
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+  })
+);
+
+app.listen(4000);
+
+console.log("GraphQL running on port 4000");
