98829567a56fe8c14f1bcce48b209043cbc3a533
Olivier Scherrer <pode.fr@gmail.com>
2022-06-08 13:02:21 +0200

    cleanup form

--- client/src/CreateToken.jsx
+++ client/src/CreateToken.jsx
@@ -6,7 +6,7 @@ export default function CreateToken() {
   const [coinData, setCoinData] = useState("");
   const navigate = useNavigate();
 
-  const [createCoin, { data, loading, error }] = useMutation(
+  const [createCoin] = useMutation(
     gql`
       mutation CreateCoin($data: String) {
         addCoin(data: $data)
@@ -25,14 +25,63 @@ export default function CreateToken() {
   }
 
   return (
-    <form>
-      <input
-        type="text"
-        onChange={(event) => setCoinData(event.target.value)}
-      />
-      <button type="button" onClick={() => addCoin(coinData)}>
-        Create
-      </button>
-    </form>
+    <div>
+      <div className="md:grid md:grid-cols-3 md:gap-6">
+        <div className="md:col-span-1">
+          <div className="px-4 sm:px-0">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+              Coin Information
+            </h3>
+            <p className="mt-1 text-sm text-gray-600">
+              Specify here the information you want to display about your
+              PokeCoin
+            </p>
+          </div>
+        </div>
+        <div className="mt-5 md:mt-0 md:col-span-2">
+          <form action="#" method="POST">
+            <div className="shadow sm:rounded-md sm:overflow-hidden">
+              <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                <div>
+                  <label
+                    htmlFor="about"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    {" "}
+                    Location{" "}
+                  </label>
+                  <div className="mt-1">
+                    <textarea
+                      id="about"
+                      name="data"
+                      rows="3"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      placeholder="40.8165487,-73.9471619"
+                      onChange={(event) => setCoinData(event.target.value)}
+                    ></textarea>
+                  </div>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Put any information that clearly identifies the PokeCoin
+                    location
+                  </p>
+                </div>
+              </div>
+              <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <button
+                  disabled={coinData.length === 0}
+                  type="button"
+                  onClick={() => addCoin(coinData)}
+                  className={
+                    "inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 text-white"
+                  }
+                >
+                  Create
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
--- client/src/ViewCoin.jsx
+++ client/src/ViewCoin.jsx
@@ -63,9 +63,7 @@ export default function ViewCoin() {
               </dd>
             </div>
             <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">
-                Localization
-              </dt>
+              <dt className="text-sm font-medium text-gray-500">Location</dt>
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                 {data.coin.data}
               </dd>
